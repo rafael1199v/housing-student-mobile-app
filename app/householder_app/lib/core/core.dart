2b3d17a64b7fd76b484a34a20421c0b941db245a
Rafael Vargas <123646379+rafael1199v@users.noreply.github.com>
2026-06-07 16:02:04 -0400

    feat: core barrel file added

---
+++ app/householder_app/lib/core/core.dart
@@ -0,0 +1,8 @@
+library;
+
+export 'config/app_config.dart';
+export 'di/injector.dart';
+export 'error/error_mapper.dart';
+export 'error/failure.dart';
+export 'session/session_notifier.dart';
+export 'storage/token_storage.dart';
