3ac12b6c6e6f8354de6500b179ebeb46889fac8d
Rafael Vargas <123646379+rafael1199v@users.noreply.github.com>
2026-06-07 20:23:21 -0400

    feat: dumb home page implementation to show a minimal auth flow

---
+++ app/householder_app/lib/features/home/presentation/pages/home_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  static const routeName = '/';
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('Home Page - Under Construction')),
+    );
+  }
+}
