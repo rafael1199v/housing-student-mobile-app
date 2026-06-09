1365aad79513c2f453f7e8cf387a2849c779a55a
Rafael Vargas <123646379+rafael1199v@users.noreply.github.com>
2026-06-09 08:44:04 -0400

    feat: field and app card components published in barrel file

--- packages/householder_design_system/lib/householder_design_system.dart
+++ packages/householder_design_system/lib/householder_design_system.dart
@@ -4,6 +4,8 @@ export 'components/brand/brand_logo.dart';
 export 'components/buttons/google_button.dart';
 export 'components/buttons/primary_button.dart';
 export 'components/inputs/app_text_field.dart';
+export 'components/inputs/field_label.dart';
+export 'components/layout/app_card.dart';
 export 'components/layout/auth_card.dart';
 export 'components/layout/labeled_divider.dart';
 export 'responsive/breakpoints.dart';
