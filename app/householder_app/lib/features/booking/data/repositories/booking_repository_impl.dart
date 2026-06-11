567bc93b8a0bcb508926030ad0257e25cc25c3a8
Rafael Vargas <123646379+rafael1199v@users.noreply.github.com>
2026-06-11 08:32:23 -0400

    feat: booking repository implementation added

---
+++ app/householder_app/lib/features/booking/data/repositories/booking_repository_impl.dart
@@ -0,0 +1,36 @@
+import '../../../../core/error/error_mapper.dart';
+import '../../domain/entities/room_bookings.dart';
+import '../../domain/repositories/booking_repository.dart';
+import '../datasources/booking_api.dart';
+import '../models/booking_mapper.dart';
+
+class BookingRepositoryImpl implements BookingRepository {
+  final BookingApi _api;
+  const BookingRepositoryImpl({required BookingApi api}) : _api = api;
+
+  @override
+  Future<RoomBookings> getRoomBookings(int roomId) async {
+    try {
+      final dto = await _api.getRoomBookings(roomId);
+      return dto.toRoomBookings();
+    } catch (error) {
+      throw ErrorMapper.map(error);
+    }
+  }
+
+  @override
+  Future<void> approveBooking(String bookingId) =>
+      _respond(() => _api.approveBooking(bookingId));
+
+  @override
+  Future<void> rejectBooking(String bookingId) =>
+      _respond(() => _api.rejectBooking(bookingId));
+
+  Future<void> _respond(Future<bool> Function() call) async {
+    try {
+      await call();
+    } catch (error) {
+      throw ErrorMapper.map(error);
+    }
+  }
+}
