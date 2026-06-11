be98a8e2c4e922c3429bcea5fed8210e74636d42
Rafael Vargas <123646379+rafael1199v@users.noreply.github.com>
2026-06-11 08:38:38 -0400

    feat: booking barrel file updated

---
+++ app/householder_app/lib/features/booking/booking.dart
@@ -0,0 +1,6 @@
+library;
+
+export 'di/booking_module.dart' show registerBookingDependencies;
+export 'domain/usecases/approve_booking_usecase.dart' show ApproveBookingUseCase;
+export 'domain/usecases/reject_booking_usecase.dart' show RejectBookingUseCase;
+export 'presentation/pages/booking_requests_page.dart' show BookingRequestsPage;
