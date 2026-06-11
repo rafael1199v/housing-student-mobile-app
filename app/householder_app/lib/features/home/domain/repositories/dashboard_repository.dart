5681ab44354b060527eb52881cda59a5b0e4d9d2
Rafael Vargas <123646379+rafael1199v@users.noreply.github.com>
2026-06-11 08:44:32 -0400

    fix: booking interaction on repository updated

--- app/householder_app/lib/features/home/domain/repositories/dashboard_repository.dart
+++ app/householder_app/lib/features/home/domain/repositories/dashboard_repository.dart
@@ -2,8 +2,4 @@ import '../entities/dashboard_summary.dart';
 
 abstract interface class DashboardRepository {
   Future<DashboardSummary> getSummary();
-
-  Future<void> acceptRequest(String bookingId);
-
-  Future<void> rejectRequest(String bookingId);
 }
