239fa470aaf346abf1b5b61599bb61ebc935fcd4
Rafael Vargas <123646379+rafael1199v@users.noreply.github.com>
2026-06-11 03:00:44 -0400

    feat: room repository implementation added

--- app/householder_app/lib/features/rooms/data/repositories/room_repository_impl.dart
+++ app/householder_app/lib/features/rooms/data/repositories/room_repository_impl.dart
@@ -1,4 +1,7 @@
+import 'package:dio/dio.dart';
+
 import '../../../../core/error/error_mapper.dart';
+import '../../domain/entities/create_room_params.dart';
 import '../../domain/entities/room_detail.dart';
 import '../../domain/repositories/room_repository.dart';
 import '../datasources/room_api.dart';
@@ -13,6 +16,45 @@ class RoomRepositoryImpl implements RoomRepository {
     try {
       final dto = await _api.getHouseholderRoomDetail(roomId);
       return dto.toRoomDetail();
+    } catch (error) {
+      throw ErrorMapper.map(error);
+    }
+  }
+
+  @override
+  Future<void> createRoom(CreateRoomParams params) async {
+    try {
+      final form = FormData();
+      form.fields.addAll([
+        MapEntry('Name', params.name),
+        MapEntry('Description', params.description),
+        MapEntry('Latitude', params.latitude.toString()),
+        MapEntry('Longitude', params.longitude.toString()),
+        MapEntry('Price', params.price.toString()),
+        MapEntry('RoomStatusId', params.statusId.toString()),
+      ]);
+
+      for (var i = 0; i < params.serviceIds.length; i++) {
+        form.fields.add(MapEntry('Services[$i].Id', '${params.serviceIds[i]}'));
+      }
+
+      for (var i = 0; i < params.policies.length; i++) {
+        final policy = params.policies[i];
+        form.fields
+          ..add(MapEntry('Policies[$i].Id', '${policy.id}'))
+          ..add(MapEntry('Policies[$i].Description', policy.description));
+      }
+
+      for (final image in params.images) {
+        form.files.add(
+          MapEntry(
+            'Images',
+            MultipartFile.fromBytes(image.bytes, filename: image.filename),
+          ),
+        );
+      }
+
+      await _api.createRoom(form);
     } catch (error) {
       throw ErrorMapper.map(error);
     }
