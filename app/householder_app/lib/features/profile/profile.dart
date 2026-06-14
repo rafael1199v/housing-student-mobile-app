17b992cceacd378b827b5189d6b57d9f94657b28
Rafael Vargas <123646379+rafael1199v@users.noreply.github.com>
2026-06-14 19:44:00 -0400

    feat: edit profile page exported

--- app/householder_app/lib/features/profile/profile.dart
+++ app/householder_app/lib/features/profile/profile.dart
@@ -1,4 +1,6 @@
 library;
 
 export 'di/profile_module.dart' show registerProfileDependencies;
+export 'domain/entities/user_profile.dart' show UserProfile;
+export 'presentation/pages/edit_profile_page.dart' show EditProfilePage;
 export 'presentation/pages/profile_page.dart' show ProfilePage;
