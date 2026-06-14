f9144d2ef4ee3a112b1e1a8f906fa0be661dced7
Rafael Vargas <123646379+rafael1199v@users.noreply.github.com>
2026-06-14 15:25:11 -0400

    feat: auth barrel file updated with confirm email page

--- app/householder_app/lib/features/auth/auth.dart
+++ app/householder_app/lib/features/auth/auth.dart
@@ -1,6 +1,7 @@
 library;
 
 export 'di/auth_module.dart' show registerAuthDependencies;
+export 'presentation/pages/confirm_email_page.dart' show ConfirmEmailPage;
 export 'presentation/pages/login_page.dart' show LoginPage;
 export 'presentation/pages/register_page.dart' show RegisterPage;
 export 'presentation/pages/registration_email_sent_page.dart'
