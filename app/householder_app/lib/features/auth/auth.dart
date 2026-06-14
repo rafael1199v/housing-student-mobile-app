71c58292a1ffb3ea443c3ea902b2c8df7544bcce
Rafael Vargas <123646379+rafael1199v@users.noreply.github.com>
2026-06-14 12:09:36 -0400

    feat: exporting new auth module pages

--- app/householder_app/lib/features/auth/auth.dart
+++ app/householder_app/lib/features/auth/auth.dart
@@ -2,3 +2,6 @@ library;
 
 export 'di/auth_module.dart' show registerAuthDependencies;
 export 'presentation/pages/login_page.dart' show LoginPage;
+export 'presentation/pages/register_page.dart' show RegisterPage;
+export 'presentation/pages/registration_email_sent_page.dart'
+    show RegistrationEmailSentPage;
