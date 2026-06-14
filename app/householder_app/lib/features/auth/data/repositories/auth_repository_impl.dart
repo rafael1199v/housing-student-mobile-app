23e544441ebb10f3e34e67f2b01b109d3e8fd1f2
Rafael Vargas <123646379+rafael1199v@users.noreply.github.com>
2026-06-14 11:38:34 -0400

    feat: auth repository implementation updated with register method

--- app/householder_app/lib/features/auth/data/repositories/auth_repository_impl.dart
+++ app/householder_app/lib/features/auth/data/repositories/auth_repository_impl.dart
@@ -4,6 +4,7 @@ import '../../domain/repositories/auth_repository.dart';
 import '../datasources/auth_api.dart';
 import '../models/credentials_mapper.dart';
 import '../models/login_dto.dart';
+import '../models/register_dto.dart';
 
 class AuthRepositoryImpl implements AuthRepository {
   const AuthRepositoryImpl({
@@ -28,6 +29,36 @@ class AuthRepositoryImpl implements AuthRepository {
         refreshToken: credentials.refreshToken,
       );
       return credentials;
+    } catch (error) {
+      throw ErrorMapper.map(error);
+    }
+  }
+
+  @override
+  Future<String> register({
+    required String email,
+    required String password,
+    required String firstName,
+    required String lastName,
+    required String phoneNumber,
+    required String nationality,
+    required String gender,
+    required String birthDate,
+  }) async {
+    try {
+      final dto = await _api.register(
+        RegisterDto(
+          email: email,
+          password: password,
+          firstName: firstName,
+          lastName: lastName,
+          phoneNumber: phoneNumber,
+          nationality: nationality,
+          gender: gender,
+          birthDate: birthDate,
+        ),
+      );
+      return dto.userId;
     } catch (error) {
       throw ErrorMapper.map(error);
     }
