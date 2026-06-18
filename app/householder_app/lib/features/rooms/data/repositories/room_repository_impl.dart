afab327ab29251326706f35e825b9b954f0e1a68
Rafael Vargas <123646379+rafael1199v@users.noreply.github.com>
2026-06-18 19:03:45 -0400

    feat: room repository implementation updated for edit room feature

--- app/householder_app/lib/features/rooms/data/repositories/room_repository_impl.dart
+++ app/householder_app/lib/features/rooms/data/repositories/room_repository_impl.dart
@@ -1,8 +1,11 @@
 import 'package:dio/dio.dart';
 
-import '../../../../core/error/error_mapper.dart';
+import '../../../../core/core.dart';
 import '../../domain/entities/create_room_params.dart';
 import '../../domain/entities/room_detail.dart';
+import '../../domain/entities/room_image.dart';
+import '../../domain/entities/selected_policy.dart';
+import '../../domain/entities/update_room_params.dart';
 import '../../domain/repositories/room_repository.dart';
 import '../datasources/room_api.dart';
 import '../models/room_detail_mapper.dart';
@@ -24,39 +27,91 @@ class RoomRepositoryImpl implements RoomRepository {
   @override
   Future<void> createRoom(CreateRoomParams params) async {
     try {
-      final form = FormData();
-      form.fields.addAll([
-        MapEntry('Name', params.name),
-        MapEntry('Description', params.description),
-        MapEntry('Latitude', params.latitude.toString()),
-        MapEntry('Longitude', params.longitude.toString()),
-        MapEntry('Price', params.price.toString()),
-        MapEntry('RoomStatusId', params.statusId.toString()),
-      ]);
-
-      for (var i = 0; i < params.serviceIds.length; i++) {
-        form.fields.add(MapEntry('Services[$i].Id', '${params.serviceIds[i]}'));
-      }
-
-      for (var i = 0; i < params.policies.length; i++) {
-        final policy = params.policies[i];
-        form.fields
-          ..add(MapEntry('Policies[$i].Id', '${policy.id}'))
-          ..add(MapEntry('Policies[$i].Description', policy.description));
-      }
-
-      for (final image in params.images) {
-        form.files.add(
-          MapEntry(
-            'Images',
-            MultipartFile.fromBytes(image.bytes, filename: image.filename),
-          ),
-        );
-      }
+      final form = _buildRoomFormData(
+        name: params.name,
+        description: params.description,
+        latitude: params.latitude,
+        longitude: params.longitude,
+        price: params.price,
+        statusId: params.statusId,
+        serviceIds: params.serviceIds,
+        policies: params.policies,
+        images: params.images,
+      );
 
       await _api.createRoom(form);
     } catch (error) {
       throw ErrorMapper.map(error);
     }
+  }
+
+  @override
+  Future<void> updateRoom(int roomId, UpdateRoomParams params) async {
+    try {
+      final form = _buildRoomFormData(
+        name: params.name,
+        description: params.description,
+        latitude: params.latitude,
+        longitude: params.longitude,
+        price: params.price,
+        statusId: params.statusId,
+        serviceIds: params.serviceIds,
+        policies: params.policies,
+        images: params.images,
+      );
+
+      for (var i = 0; i < params.keptImageIds.length; i++) {
+        form.fields
+            .add(MapEntry('KeptImageIds[$i]', '${params.keptImageIds[i]}'));
+      }
+
+      await _api.updateRoom(roomId, form);
+    } catch (error) {
+      throw ErrorMapper.map(error);
+    }
+  }
+
+  FormData _buildRoomFormData({
+    required String name,
+    required String description,
+    required double latitude,
+    required double longitude,
+    required double price,
+    required int statusId,
+    required List<int> serviceIds,
+    required List<SelectedPolicy> policies,
+    required List<RoomImage> images,
+  }) {
+    final form = FormData();
+    form.fields.addAll([
+      MapEntry('Name', name),
+      MapEntry('Description', description),
+      MapEntry('Latitude', latitude.toString()),
+      MapEntry('Longitude', longitude.toString()),
+      MapEntry('Price', price.toString()),
+      MapEntry('RoomStatusId', statusId.toString()),
+    ]);
+
+    for (var i = 0; i < serviceIds.length; i++) {
+      form.fields.add(MapEntry('Services[$i].Id', '${serviceIds[i]}'));
+    }
+
+    for (var i = 0; i < policies.length; i++) {
+      final policy = policies[i];
+      form.fields
+        ..add(MapEntry('Policies[$i].Id', '${policy.id}'))
+        ..add(MapEntry('Policies[$i].Description', policy.description));
+    }
+
+    for (final image in images) {
+      form.files.add(
+        MapEntry(
+          'Images',
+          MultipartFile.fromBytes(image.bytes, filename: image.filename),
+        ),
+      );
+    }
+
+    return form;
   }
 }
