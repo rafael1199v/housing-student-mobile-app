f745d8b515275727194ff130ec0abef27bfeed1c
Rafael Vargas <123646379+rafael1199v@users.noreply.github.com>
2026-06-19 08:13:32 -0400

    feat: google-sign-in service implementation for both platforms added

--- app/householder_app/lib/core/platform/google_sign_in_service_factory.dart
+++ app/householder_app/lib/core/platform/google_sign_in_service_factory.dart
@@ -1,5 +1,7 @@
 import 'google_sign_in_service.dart';
-import 'google_sign_in_service_stub.dart';
+import 'google_sign_in_service_stub.dart'
+    if (dart.library.js_interop) 'google_sign_in_service_web.dart'
+    if (dart.library.io) 'google_sign_in_service_mobile.dart';
 
 GoogleSignInService createGoogleSignInService({
   String? webClientId,
