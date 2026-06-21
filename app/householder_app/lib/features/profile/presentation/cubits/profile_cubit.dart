d1c8347ec4cdc6dbac33b855e7f0fdd078b6bbe2
Rafael Vargas <123646379+rafael1199v@users.noreply.github.com>
2026-06-21 12:55:30 -0400

    feat: flutter foundation package added to the profile cubit

--- app/householder_app/lib/features/profile/presentation/cubits/profile_cubit.dart
+++ app/householder_app/lib/features/profile/presentation/cubits/profile_cubit.dart
@@ -1,6 +1,5 @@
-import 'dart:typed_data';
-
 import 'package:equatable/equatable.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../../core/core.dart';
