6e967d8dcde14f646bac1cbea3ad5e89ea9efb5b
Rafael Vargas <123646379+rafael1199v@users.noreply.github.com>
2026-06-21 14:22:54 -0400

    feat: auth feature removed from the householder app

--- app/householder_app/lib/features/auth/domain/entities/google_auth_result.dart
+++
@@ -1,8 +0,0 @@
-import 'credentials.dart';
-
-class GoogleAuthResult {
-  const GoogleAuthResult({required this.isNewUser, this.credentials});
-
-  final bool isNewUser;
-  final Credentials? credentials;
-}
