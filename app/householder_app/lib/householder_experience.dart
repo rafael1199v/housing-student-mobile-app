b7681484623e3bd2534e18a156cfaa73b39ad913
Rafael Vargas <123646379+rafael1199v@users.noreply.github.com>
2026-06-20 23:37:12 -0400

    feat: householder experience barrel file updated

---
+++ app/householder_app/lib/householder_experience.dart
@@ -0,0 +1,5 @@
+library;
+
+export 'core/di/injector.dart' show registerHouseholderDependencies;
+export 'core/router/app_router.dart'
+    show householderExperienceRoutes, householderInitialLocation;
