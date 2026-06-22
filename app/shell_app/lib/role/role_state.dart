1466cea6c96f9c302acb3f2d0bb48d0c16a02fe6
Rafael Vargas <123646379+rafael1199v@users.noreply.github.com>
2026-06-22 14:39:10 -0400

    feat: role cubit updated to suport role redirection

--- app/shell_app/lib/role/role_state.dart
+++ app/shell_app/lib/role/role_state.dart
@@ -1,15 +1,20 @@
 part of 'role_cubit.dart';
 
 class RoleState extends Equatable {
-  const RoleState({this.heldRoles = const [], this.activeRole});
+  const RoleState({
+    this.heldRoles = const [],
+    this.activeRole,
+    this.loaded = false,
+  });
 
   final List<AppRole> heldRoles;
   final AppRole? activeRole;
+  final bool loaded;
 
   bool get canSwitch => heldRoles.length > 1;
 
   List<AppRole> get assignable => RoleHierarchy.assignableFor(heldRoles);
 
   @override
-  List<Object?> get props => [heldRoles, activeRole];
+  List<Object?> get props => [heldRoles, activeRole, loaded];
 }
